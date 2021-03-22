89ce758c813a7a8544cb5baec68803d80f700db7
Mrjacint <mrjacint@gmail.com>
2021-03-22 11:53:57 +0000

    Register serviceworker & downsize portfolio imgs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-import './assets/css/style.css';
-import './assets/vendor/bootstrap/css/bootstrap.min.css';
-import './assets/vendor/icofont/icofont.min.css';
-import './assets/vendor/boxicons/css/boxicons.min.css';
+import "./assets/css/style.css";
+import "./assets/vendor/bootstrap/css/bootstrap.min.css";
+import "./assets/vendor/icofont/icofont.min.css";
+import "./assets/vendor/boxicons/css/boxicons.min.css";
 // import './assets/vendor/venobox/venobox.css';
 // import './assets/vendor/owl.carousel/assets/owl.carousel.min.css';
-
 
 ReactDOM.render(
   <BrowserRouter>
@@ -18,10 +18,15 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.register();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
